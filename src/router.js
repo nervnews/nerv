f83b745ca31662fe3e4d1e51bd840f0a761d7180
MynahMarie <mynah.marie@gmail.com>
2018-01-15 12:09:00 +0200

    Initialize view engine
    Modify router to render home page, added helpers folder and index.js file, modified articles.hbs page so that variable names fit the data we are receiving from api
    Relates #15

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send('<h1>You are in the homepage</h1>');
+  res.render('home');
 })
 
 router.get("*", (req, res)=> {
