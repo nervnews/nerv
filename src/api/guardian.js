63891cde7c147d5b9af0081ca27c2a2c837e8f38
Ghassan <ghassan.maslamani@gmail.com>
2018-01-16 12:04:42 +0200

    Filter for the standardizing the data as well as to get the full results from the Guardian api Relates #25

--- src/api/guardian.js
+++ src/api/guardian.js
@@ -1,14 +1,24 @@
 const Guardian = require("guardian-js");
 const guardian_filter = require("./guardian_filter");
-const guardianKey = (query, cb) => {
+let allArticles = [];
+const guardianKey = (query, page, cb) => {
   const welvonAPI = new Guardian(process.env.GUARDIAN_KEY, false);
 
   welvonAPI.content
     .search(query, {
-      "show-fields": "headline,trailText,thumbnail,bodyText,page,lastModified"
+      "show-fields": "headline,trailText,thumbnail,bodyText,page,lastModified",
+      page: page
     })
     .then(response => {
-      cb(JSON.parse(response.body.response));
+      json_res = JSON.parse(response.body).response;
+      const { currentPage, pageSize } = json_res;
+      console.log(currentPage, pageSize);
+      if (currentPage + 1 <= pageSize) {
+        allArticles = allArticles.concat(guardian_filter(json_res));
+        guardianKey(query, currentPage + 1, cb);
+      } else {
+        cb(allArticles.concat(guardian_filter(json_res)));
+      }
     });
 };
 
--- src/api/guardian_filter.js
+++ src/api/guardian_filter.js
@@ -1,1 +1,14 @@
-module.exports = response => {};
+module.exports = response => {
+  console.log(response.results.constructor);
+  let results = response.results.reduce((articles, article) => {
+    articles.push({
+      id: article.id,
+      image: article.fields.thumbnail,
+      headline: article.fields.headline,
+      trailText: article.fields.trailText,
+      bodyText: article.fields.bodyText
+    });
+    return articles;
+  }, []);
+  return results;
+};
