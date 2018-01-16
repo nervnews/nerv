384e1654221f50ebbd6904045fd6224b7bef4963
sophieeejw <jungwon.lim.0210@gmail.com>
2018-01-16 11:49:13 +0200

    Update guardian.js

--- src/api/guardian.js
+++ src/api/guardian.js
@@ -1,14 +1,10 @@
 const Guardian = require("guardian-js");
 const guardian_filter = require("./guardian_filter");
 
-<<<<<<< HEAD
-const guardianListing = (query, cb) => {
-  const api = new Guardian(guardian_key, false);
-=======
-const guardianKey = (query, cb) => {
-  const welvonAPI = new Guardian(process.env.GUARDIAN_KEY, false);
->>>>>>> e232b0b6152dd9ed502335bfefebb134f967432f
 
+const guardianListing = (query, cb) => {
+ 
+  const welvonAPI = new Guardian(process.env.GUARDIAN_KEY, false);
   welvonAPI.content
     .search(query, {
       "show-fields": "headline,trailText,thumbnail,bodyText,page,lastModified"
