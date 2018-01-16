834f7fddf912b61b07cfe7a5867151a45f42df90
Ghassan <ghassan.maslamani@gmail.com>
2018-01-16 12:38:17 +0200

    matching the new data structure with the rendering

--- src/router.js
+++ src/router.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const guardianListing = require("./api/guardian")
+const guardianListing = require("./api/guardian");
 const bodyParser = require("body-parser");
 
 router.get("/", (req, res) => {
-  res.render('home');
-})
-
+  res.render("home");
+});
 
 router.post("/articles", (req, res) => {
-query = req.body.search;
-    guardianListing(query, (results) => {
-      const articles = results.response.results
-        res.status(200).render('articles', { articles });
-      })
-})
+  query = req.body.search;
+  guardianListing(query, results => {
+    const articles = results;
+    res.status(200).render("articles", { articles });
+  });
+});
 
 router.get("*", (req, res) => {
   res.send("<h1>404 PAGE NOT FOUND</h1>");
