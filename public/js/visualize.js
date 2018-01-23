89f3e0d2bf6aee542f68d6c48199e8228b41f5af
“Sophia” <jungwon.lim.0210@gmail.com>
2018-01-23 16:00:30 +0200

    Converting ES6 frontend code to ES5 Relates #40'

--- public/js/visualize.js
+++ public/js/visualize.js
@@ -1,9 +1,9 @@
-const urlParams = new URLSearchParams(window.location.search);
-const url = `visualize/${urlParams.getAll('articleID')}`;
+var urlParams = new URLSearchParams(window.location.search);
+var url = 'visualize/'+urlParams.getAll('articleID');
 
 fetchGET(url, function(err, data){
   if (err) {
-    alert("Could not get data requested. Please try search another key word!");
+    alert('Could not get data requested. Please try search another key word!');
   } else {
     // This is the time to process the data using D3.js
     // By adding a console log, it's possible to see the data that is returned
@@ -11,28 +11,28 @@ fetchGET(url, function(err, data){
     // The following is a proof of concept on how we render this data using D3.
     var data = data.splice(0, 15);
 
-    const svg = d3.select('svg');
-    const maxSize = data[0].size;
-    const minSize = data[data.length - 1].size;
-    const width = window.innerWidth * 0.9;
-    const height = window.innerHeight * 0.9;
+    var svg = d3.select('svg');
+    var maxSize = data[0].size;
+    var minSize = data[data.length - 1].size;
+    var width = window.innerWidth * 0.9;
+    var height = window.innerHeight * 0.9;
 
-    let radius = 4;
-    let fontSize = 4;
-    const scaleIt = width < height ? width : height;
+    var radius = 4;
+    var fontSize = 4;
+    var scaleIt = width < height ? width : height;
 
-    const radiusScale = d3
+    var radiusScale = d3
       .scaleSqrt()
       .domain([minSize, maxSize])
       .range([scaleIt / 20, scaleIt / 5]);
 
-    const drag = d3
+    var drag = d3
       .drag()
       .on('start', dragstarted)
       .on('drag', dragged)
       .on('end', dragended);
 
-    const group = svg
+    var group = svg
       .selectAll('g')
       .data(data)
       .enter()
@@ -41,46 +41,46 @@ fetchGET(url, function(err, data){
 
     group
       .append('circle')
-      .attr('cx', d => width / 2)
-      .attr('cy', d => height / 2)
-      .attr('r', d => 5)
-      .attr('fill', d => d.colorScore);
+      .attr('cx',  width / 2)
+      .attr('cy',  height / 2)
+      .attr('r',  5)
+      .attr('fill', function(d) {return d.colorScore});
 
     group
       .append('text')
-      .attr('text-anchor', d => 'middle')
-      .attr('x', (d, i) => width / 2)
-      .attr('y', (d, i) => height / 2)
-      .text(d => d.word);
+      .attr('text-anchor', 'middle')
+      .attr('x',  width / 2)
+      .attr('y',  height / 2)
+      .text(function(d) {return d.word});
 
     nodesCircles = d3.selectAll('circle');
     nodesTexts = d3.selectAll('text');
 
     // simulation is a collection of forces
     // that going to affect our circles
-    const simulation = d3
+    var simulation = d3
       .forceSimulation()
       .force('x', d3.forceX(width / 2).strength(0.05))
       .force('y', d3.forceY(height / 1.7).strength(0.05))
-      .force('collide', d3.forceCollide(d => radiusScale(d.size)));
+      .force('collide', d3.forceCollide(function(d) {return radiusScale(d.size)}));
     simulation.alphaDecay([0.001]);
     simulation.nodes(data).on('tick', ticked);
     function ticked() {
       radius += 2;
       fontSize += 1;
       nodesCircles
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y)
-        .attr('r', (d) => {
+        .attr('cx', function(d){return d.x})
+        .attr('cy', function(d){return d.y})
+        .attr('r', function(d){
           if (radius >= radiusScale(d.size)) return radiusScale(d.size);
           return radius;
         });
       nodesTexts
-        .attr('x', d => d.x)
-        .attr('y', d => d.y)
-        .attr('style', (d) => {
-          if (fontSize >= radiusScale(d.size) / 2) return `font-size: ${radiusScale(d.size) / 2}`;
-          return `font-size: ${fontSize}px`;
+        .attr('x', function(d){return d.x})
+        .attr('y', function(d){return d.y})
+        .attr('style', function(d) {
+          if (fontSize >= radiusScale(d.size) / 2) return ('font-size: '+radiusScale(d.size) / 2);
+          return 'font-size: '+fontSize+'px';
         });
     }
 
