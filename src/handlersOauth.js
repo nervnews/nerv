fbbe8a24e6931082d677c2afa8b7ba71528f2fa0
Ghassan <ghassan.maslamani@gmail.com>
2018-01-24 20:07:42 +0200

    All the necessary changes to add the visualize all feature Relates and Closes #48

--- src/handlersOauth.js
+++ src/handlersOauth.js
@@ -1,11 +1,11 @@
 exports.dashboard = (req, res) => {
-    const user = req.user;
-    const username = user.name.givenName;
-
-    res.status(200).render("dashboard", { username })
-}
+  const { user } = req;
+  const username = user.name.givenName;
+  const state = { text: 'Logout', link: '/oauth/logout' };
+  res.status(200).render('dashboard', { username, state });
+};
 
 exports.logout = (req, res) => {
-    req.logout();
-    res.redirect("/");
-}
+  req.logout();
+  res.redirect('/');
+};
